dd50cc3798bd02594dac5923eedc20ddeb7940a5
Sam <sam.fordham1@gmail.com>
2020-08-20 12:04:53 +0100

    Completed Question 8, create Person component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import PageTitle from './components/pageTitle.js';
 import HelloKitty from './components/helloKitty.js'
+import Person from './components/person.js'
 
 function App() {
   return (
     <div className="App">
           <PageTitle />
           <HelloKitty />
+          <Person />
           <a
           className="App-link"
           href="https://reactjs.org"
