2b7f98d5bee8783d4e956f6ad820c0e85a69da6d
Sam <sam.fordham1@gmail.com>
2020-08-20 11:39:17 +0100

    Can't find the problem with hello.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PageTitle from './components/pageTitle.js';
+import HelloKitty from './components/helloKitty.js'
 
 function App() {
   return (
     <div className="App">
           <PageTitle />
+          <HelloKitty />
           <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/helloKitty.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hello from 'hello.js'
+import Kitty from 'kitty.js'
+
+const HelloKitty = () => {
+    return (
+        <div>
+            <p>
+            <Hello />
+            <Kitty />
+            </p>
+        </div>
+    )
+}
+
+export default HelloKitty
