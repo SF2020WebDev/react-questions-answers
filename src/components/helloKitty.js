eb0c3becf66292599474b538495ffb3bf0f01c50
Sam <sam.fordham1@gmail.com>
2020-08-20 11:47:04 +0100

    Completed Question 7 Hello Kitty

--- src/components/helloKitty.js
+++ src/components/helloKitty.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import Hello from 'hello.js'
-import Kitty from 'kitty.js'
+import Hello from './hello.js'
+import Kitty from './kitty.js'
 
 const HelloKitty = () => {
     return (
         <div>
-            <p>
-            <Hello />
-            <Kitty />
-            </p>
+            <Hello /> <Kitty />
         </div>
     )
 }
